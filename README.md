41869d5ca796a3a342512618d1a8ad3918d4c57a
Brian Kelly <GitHub@Brian.Kelly.name>
2015-08-07 22:55:07 -0400

    Updated README.md
    Preliminary version of make-debian-package.sh
    
    Created initial package, Linux-Volume-Scripts_0.0-1.deb

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Linux_Volume_Scripts
-<!--- Version: 0.0.1 --->
+<!--- Project=Linux-Volume-Scripts --->
+<!--- MajorVersion=0 --->
+<!--- MinorVersion=0 --->
+<!--- PackageVersion=1 --->
+<!--- MaintainerName="Brian Kelly" --->
+<!--- MaintainerEmail=Github@Brian.Kelly.name --->
+<!--- Depends="perl (>= 5.18.2), mdadm (>= 3.2.5), lvm2 (>= 2.02.98)" --->
+<!--- Description="Scripts to help manage LVM on software RAID(1)" --->
 
 Scripts for managing LVM volumes on software RAID
+
+> This is my collection of scripts for managing my disk volumes (using
+> LVM2 on RAID1 software mirrors).
 
 --[Brian Kelly](https://github.com/hiwaybk)
--- make-debian-package.sh
+++ make-debian-package.sh
@@ -3,7 +3,53 @@
 # Package creation script created based off info from:
 #   http://ubuntuforums.org/showthread.php?t=910717
 
-NAME=`pwd`
-NAME=`dirname "${NAME}"`
-NAME=`basename "${NAME}"`
-echo $NAME
+####
+#### Make sure we're running in the correct directory.
+####
+
+cd `dirname "${0}"`
+
+####
+#### Pull some settings from README.md
+####
+
+SETTINGS=`cat README.md`
+SETTINGS=`echo "${SETTINGS}" | grep '^<!---'`
+SETTINGS=`echo "${SETTINGS}" | sed -e 's/^<!--- *//' -e 's/ *--->$//'`
+eval "${SETTINGS}"
+
+####
+#### Set the package name
+####
+
+PACKAGE="${Project}"
+VERSION="${MajorVersion}.${MinorVersion}-${PackageVersion}"
+NAME="${PACKAGE}_${VERSION}"
+
+####
+#### Create the file structure for the package
+####
+
+mkdir -p "${NAME}/usr/local/bin"
+mkdir -p "${NAME}/DEBIAN"
+
+####
+#### Create the package control file
+####
+
+CONTROL="${NAME}/DEBIAN/control"
+echo "Package: ${PACKAGE}" > "${CONTROL}"
+echo "Version: ${VERSION}" >> "${CONTROL}"
+echo "Section: base" >> "${CONTROL}"
+echo "Priority: optional" >> "${CONTROL}"
+echo "Architecture: i386" >> "${CONTROL}"
+echo "Depends: ${Depends}" >> "${CONTROL}"
+echo "Maintainer: ${MaintainerName} <${MaintainerEmail}>" >> "${CONTROL}"
+echo "Description: ${Description}" >> "${CONTROL}"
+cat README.md | grep '^> ' | sed -e 's/^> / /' >> "${CONTROL}"
+
+####
+#### 
+####
+
+dpkg-deb --build "${NAME}" && rm -rf "${NAME}"
